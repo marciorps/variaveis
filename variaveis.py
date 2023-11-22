8d03f37d654400083791ee6f66680b060c81faf6
marciorps <marciorps@yahoo.com,.br>
2023-11-22 10:11:28 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,36 @@
+velocidade_internet = 10
+print(velocidade_internet)
+
+# Quais os tipos de dados podemos armazenar na memoria do computador?
+#Números
+
+velocidade_internet = 10
+
+# Valores boleanos
+esta_aberta = True
+
+#strings
+slogan = 'Feito é melor que perfeito!'
+slogan = 'Feito é melhor que perfeito!'
+a,b,c,d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+
+# Desafio
+#Copie e cole as seguintes linhas de códgo para
+#seu editor de códgo e descubra qual é o tipo de cada um desse dados
+
+variavel_1 = 25.87
+variavel_2 = True
+variavel_3 = "Bom dia!"
+variavel_4 = 15
+
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
+
